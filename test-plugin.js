789d1dc6164b8ac98aa576e6275b65b5367340c4
Jason Dugdale <jason@frontapp.com>
2019-09-03 09:26:53 -0700

    Add new tag

--- test-plugin.js
+++ test-plugin.js
@@ -140,6 +140,10 @@ function tag() {
   Front.attachTag('star');
 }
 
+function newTag() {
+  Front.attachTag('new_tag_' + Date.now());
+}
+
 function addTopic() {
   Front.addTopic({
       type: 'web',
